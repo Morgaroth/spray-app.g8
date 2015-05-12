7986ac2b30ba801c84783ffef085836c3f3689ea
Mateusz Jaje <Morgaroth@users.noreply.github.com>
2015-05-13 00:22:06 +0200

    Update application.scala

--- src/main/g8/src/main/scala/$package$/application.scala
+++ src/main/g8/src/main/scala/$package$/application.scala
@@ -3,12 +3,15 @@ package $package$
 import akka.actor.ActorSystem
 import spray.routing._
 
+trait Backend {
+  val system = ActorSystem("LOCAL")
+}
+
 trait WebApi extends Directives {
+  this: Backend =>
   val routes: Route = get {
     complete("Hello from $name$ application")
   }
 }
 
-trait Backend {
-  val system = ActorSystem("LOCAL")
-}
+
